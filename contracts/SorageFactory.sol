a6fb89346652a17bf6406f85639afe495bc441ae
Heritage <ireanuoluwah@gmail.com>
2025-06-12 22:13:38 +0100

    Called simple storage store and retrieve function inside storage factory

--- contracts/SorageFactory.sol
+++ contracts/SorageFactory.sol
@@ -3,8 +3,15 @@
 pragma solidity ^0.8.12;
 import './SimpleStorage.sol';
 contract StorageFactory{
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public simpleStorageArray;
     function createSimpleStorageContract( ) public{
-        simpleStorage = new SimpleStorage();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+    function sfStore (uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public{
+        SimpleStorage(simpleStorageArray[_simpleStorageIndex]).store(_simpleStorageNumber);
+    }
+    function sfGet (uint256 _simpleStorageIndex) public view returns(uint256){
+        return SimpleStorage(simpleStorageArray[_simpleStorageIndex]).retrieve();
     }
 }
